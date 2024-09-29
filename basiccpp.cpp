6db65e509331ee5580f23c48861cdc711b3633e5
Ashish2004089 <131390150+Ashish2004089@users.noreply.github.com>
2024-09-29 17:40:15 +0530

    Create basiccpp.cpp

---
+++ basiccpp.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ll long long
+
+#ifndef ONLINE_JUDGE
+
+#else
+#define deb(x...)
+#endif
+const int mod = 998244353;
+const int N = 2 * 1e5 + 5;
+
+
+void solve() {
+
+
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	solve();
+	return 0;
+}
